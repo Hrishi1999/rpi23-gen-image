6a1c718bf5fd102c662cafb64b351519000ace80
Hrishi1999 <hrishipatel99@gmail.com>
2017-06-15 20:16:04 +0530

    For DM

--- rpi23-gen-image.sh
+++ rpi23-gen-image.sh
@@ -126,6 +126,7 @@ ENABLE_DBUS=${ENABLE_DBUS:=true}
 ENABLE_HWRANDOM=${ENABLE_HWRANDOM:=true}
 ENABLE_MINGPU=${ENABLE_MINGPU:=false}
 ENABLE_XORG=${ENABLE_XORG:=true}
+ENABLE_DM=${ENABLE_DM:="lightdm"}
 ENABLE_WM=${ENABLE_WM:="sucrose"}
 ENABLE_RSYSLOG=${ENABLE_RSYSLOG:=true}
 ENABLE_USER=${ENABLE_USER:=true}
@@ -419,7 +420,7 @@ fi
 
 # Add user defined window manager package
 if [ -n "$ENABLE_WM" ] ; then
-  APT_INCLUDES="${APT_INCLUDES},${ENABLE_WM}"
+  APT_INCLUDES="${APT_INCLUDES},${ENABLE_WM},${ENABLE_DM}"
 
   # Enable xorg package dependencies
   ENABLE_XORG=true
